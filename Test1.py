dd90513428d10aee66b5d8f5b3665595dc31852f
Milind Chaukate <milindckt45@gmail.com>
2024-12-05 16:38:01 +0530

    commit1

--- Test1.py
+++ Test1.py
@@ -1,12 +1,2 @@
-from itertools import count
-
-
-def count_vowels(string):
-    vowels = "aeiouAEIOU"
-    count = 0
-    for char in string:
-        if char in vowels:
-            count = count + 1
-    return count
-string = str(input("Enter the string: "))
-print(count_vowels(string))
+add = lambda x, y : x + y
+print(add(5,6))
